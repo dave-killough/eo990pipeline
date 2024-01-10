14953a8e9ea10575adca05af15264e76c4b11605
dave-killough <dave-killough>
2024-01-10 23:55:12 +0000

    test

---
+++ Ingest.ipynb.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %pip install datamachine
+
+# COMMAND ----------
+
+import datamachine as dm
+
+# COMMAND ----------
+
+fam = dm.import_notebook('fam')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Markdown cell
+
+# COMMAND ----------
+
+
